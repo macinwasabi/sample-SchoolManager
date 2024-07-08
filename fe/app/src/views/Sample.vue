b170699ab9b1911f4e1254233e336ff08366b7b9
わさび <96158554+macinwasabi@users.noreply.github.com>
2024-07-08 09:43:43 +0900

    プロジェクトを作成

---
+++ fe/app/src/views/Sample.vue
@@ -0,0 +1,28 @@
+<template>
+  <Student :students="students" />
+  <Teacher :teachers="teachers" />
+</template>
+
+<script setup>
+import { ref } from "vue"
+import Student from '../components/Student.vue'
+import Teacher from '../components/Teacher.vue'
+
+const students = ref([
+  {
+    no: 1,
+    familyName: "te",
+    firstName: "st"
+  }
+])
+
+const teachers = ref([
+  {
+    no: 1,
+    familyName: "鈴木",
+    firstName: "先生"
+  }
+])
+
+// TODO:PromiseとasyncでAPIを呼び出したい
+</script>
